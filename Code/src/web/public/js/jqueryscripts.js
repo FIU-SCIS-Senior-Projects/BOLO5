7a056f13536f60e8ad2ca6909bc83822680e7324
nmuno013 <nmuno013@fiu.edu>
2016-07-03 22:38:02 -0400

    Created validations for Propulsion Theft-Boat, model and serial number

--- Code/src/web/public/js/jqueryscripts.js
+++ Code/src/web/public/js/jqueryscripts.js
@@ -7,22 +7,22 @@ jQuery(function(){
     // Uppercase first letter each word Last Name
     $("#lastName[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     // Uppercase first letter each word First Name
     $("#firstName[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     //Validate numbers in format 99/99/99 Date of Birth
     $("#dob").mask("99/99/9999",{placeholder:""});
     //Transform characters to uppercase
     $("#dlNumber[type=text]").keyup(function(){
-        $(this).val( $(this).val().toUpperCase() );
+        $(this).val( $(this).val().toUpperCase());
     });
     //Transform characters to uppercase Address
     $("#address[type=text]").keyup(function(){
-        $(this).val( $(this).val().toUpperCase() );
+        $(this).val( $(this).val().toUpperCase());
     });
     //Validate numbers in format 99999 Xip Code
     $("#zipCode").mask("99999",{placeholder:""});
@@ -39,30 +39,30 @@ jQuery(function(){
     // Uppercase first letter each word Vehicke Make
     $("#vehicleMake[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     // Uppercase first letter each word Vehicle Model
     $("#vehicleModel[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     // Uppercase first letter each word Vehicle Trim
     $("#vehicleTrim[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     // Uppercase first letter each word Vehicle Color
     $("#vehicleColor[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     //Transform characters to uppercase Vehicle License Plate
     $("#vehicleLicensePlate[type=text]").keyup(function(){
-        $(this).val( $(this).val().toUpperCase() );
+        $(this).val( $(this).val().toUpperCase());
     });
     //Transform characters to uppercase Vehicle VIN
     $("#vehicleIdNumber[type=text]").keyup(function(){
-        $(this).val( $(this).val().toUpperCase() );
+        $(this).val( $(this).val().toUpperCase());
     });
 
 //Validations for Theft-Boat VESSEL
@@ -72,25 +72,36 @@ jQuery(function(){
     // Uppercase first letter each word Boat Manufacturer
     $("#boatManufacturer[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     // Uppercase first letter each word Boat Type
     $("#boatType[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     // Uppercase first letter each word Boat Color
     $("#boatColor[type='text']").keyup(function(evt){
         var txt = $(this).val();
-        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase( ); }));
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
     });
     //Transform characters to uppercase Hull ID
     $("#boatHullIdNumber[type=text]").keyup(function(){
-        $(this).val( $(this).val().toUpperCase() );
+        $(this).val( $(this).val().toUpperCase());
     });
     //Transform characters to uppercase Vehicle VIN
     $("#boatRegistrationNumberNu[type=text]").keyup(function(){
-        $(this).val( $(this).val().toUpperCase() );
+        $(this).val( $(this).val().toUpperCase());
     }).mask("9999aa",{placeholder:""});
+
+//Validations for Theft-Boat PROPULSION
+    // Uppercase first letter each word Propulsion Model
+    $("#propulsionModel[type='text']").keyup(function(evt){
+        var txt = $(this).val();
+        $(this).val(txt.replace(/^(.)|\s(.)/g, function($1){ return $1.toUpperCase();}));
+    });
+    //Transform characters to uppercase Propulsion Serial Number
+    $("#propulsionSerialNumber[type=text]").keyup(function(){
+        $(this).val( $(this).val().toUpperCase());
+    });
 
 });
